e9afb5618dd09a56ae7aaef63f3f8498057fcc05
Sandile Kitayi The Developer <ksandile127@gmail.com>
2024-03-14 13:09:16 +0200

    updated JavaScript
    Created a function called validdateSyntax, get go get my input on HTML which is "petInput" and then access the current value entered on my input field. And I had to ensure that the input string starts with and is followed by four digits which are (`\d{4}`) then followed by one or more alphanumeric characters (`[A-Za-zO-9]+`).

--- Module_1_SANKIT341_WFC2401_GroupA_SandileKitayi_JSL01-main (1)/script.js
+++ Module_1_SANKIT341_WFC2401_GroupA_SandileKitayi_JSL01-main (1)/script.js
@@ -4,9 +4,20 @@ function validateSyntax() {
     let result = ''; // Placeholder for validation result
 
     // TODO: Write your validation logic here
+    let valid = /^pet_\d{4}[A-Za-z0-9]+$/;
         // Check if input starts with 'pet_' and followed by alphanumeric characters
+    if (valid.test(input)) {
+        result = 'Valid Syntax';
+    } else {
+        result = 'Invalid Syntax';
+    }
 
-            document.getElementById('result').innerText = result;
+    document.getElementById('result').innerText = result;
+
+    // setting time out function
+    setTimeout(function() {
+        document.getElementById('result').innerText = ''; //sets the innerText of the result paragraph element to an empty string, effectively clearing the result after 3 seconds (3000 milliseconds). 
+    }, 3000);
 }
 
 
